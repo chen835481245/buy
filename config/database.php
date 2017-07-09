af8643a70f2f7dd8ae5f5ace35760d5c2af5112a
chenhao1 <chenhao1@youzan.com>
2017-07-10 00:10:16 +0800

    database

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'taobaoapi'),
+            'database' => env('DB_DATABASE', 'coin'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
